21e19e3704a1e93a65a9dc961f63a656e5ea3d09
Ashley Rooth <ashley.rooth@yahoo.com>
2026-04-13 15:29:49 +0200

    first

---
+++ src/sample/main.c
@@ -0,0 +1,34 @@
+#include <nrfx.h>
+#include <nrf5340_application.h>
+#include <nrfx_config.h>
+#include <nrf.h>
+#include <nrf_gpio.h>
+#include <nrfx_systick.h>
+#include <nrfx_uarte.h>
+
+/*By Daniel Morberg*/
+/*'Requires 'NRFX_SYSTICK_ENABLED 1' in nrfx_config_nrf5340_application.h and nrfx_systic.c included*/
+/*Ni kan behöva köra en "make clean" efter ni inkluderat en ny .c fil. */
+
+/*
+From the NRF5340DK documentation:
+
+Button 1    P0.23   i.e. 23
+Button 2    P0.24
+Button 3    P0.08
+Button 4    P0.09
+LED 1       P0.28   i.e. 28
+LED 2       P0.29
+LED 3       P0.30
+LED 4       P0.31
+
+*/
+
+#define LED1 28
+#define LED2 29
+#define BUTTON1 23
+#define BUTTON2 24
+
+int main(void){
+
+}
