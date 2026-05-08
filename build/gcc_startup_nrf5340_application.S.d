e70c8238b4ace83f4c8d756499827700cd08e679
softblueish <stoplooking@atmy.email>
2026-05-08 09:20:26 +0200

    It works yay :)

---
+++ build/gcc_startup_nrf5340_application.S.d
@@ -0,0 +1,2 @@
+C:/Users/arh25001/nrfx-school/src/sample/../../build/gcc_startup_nrf5340_application.S.o: \
+ C:/Users/arh25001/nrfx-school/mdk/gcc_startup_nrf5340_application.S
--- src/sample/main.c
+++ src/sample/main.c
@@ -4,6 +4,7 @@
 #include <nrf.h>
 #include <nrf_gpio.h>
 #include <nrfx_systick.h>
+#include <math.h>
 #include <nrfx_uarte.h>
 #include "BSTree.h"
 #include "uarte-commands.h"
@@ -28,34 +29,106 @@ LED 4       P0.31
 
 */
 
+#define TEST_SIZE 6
+const int tree_size[] = {3, 7, 15, 31, 63, 127};
+
 int main(void){
     init_uarte();
+    nrfx_systick_init();
     clear_screen();
     BSTree tree = create_empty_tree();
-    insert_sorted(&tree, 1);
-    insert_sorted(&tree, 2);
-    insert_sorted(&tree, 3);
-    insert_sorted(&tree, 4);
-    insert_sorted(&tree, 5);
-    insert_sorted(&tree, 6);
-    insert_sorted(&tree, 7);
-    print_string("Preorder: ");
-    print_preorder(tree);
+    int first = 0;
+    int unbalanced_speed[TEST_SIZE] = {0, 0, 0, 0, 0, 0};
+    print_string("Testing unbalanced trees of values ");
+    for(int i = 0; i < TEST_SIZE; i++) {
+        print_int(tree_size[i]);
+        if(i != TEST_SIZE - 1)
+            print_string(", ");
+        else
+            print_string(".");
+    }
     print_newline();
-    print_string("Depth: ");
-    print_int(depth(tree));
+    for(int i = 0; i < TEST_SIZE; i++) {
+        print_string("Creating tree for tree size: ");
+        print_int(tree_size[i]);
+        print_newline();
+        for(int j = 1; j <= tree_size[i]; j++) {
+            insert_sorted(&tree, j);
+        }
+        print_string("Starting search for ");
+        print_int(tree_size[i]);
+        print_string(" in tree...");
+        print_newline();
+        nrf_systick_val_clear();
+        unsigned int pretime = nrf_systick_val_get();
+        find(tree, tree_size[i]);
+        unsigned int posttime = nrf_systick_val_get();
+        print_string("Value found after ");
+        print_int((pretime - posttime));
+        print_string(" ticks!");
+        unbalanced_speed[i] = (pretime - posttime);
+        if(first == 0)
+        {
+            first = (pretime - posttime);
+        }
+        else
+        {
+            print_string(" ... which is around ");
+            print_int(ceil((pretime - posttime)/(double)first));
+            print_string("x slower compared to the first (unbalanced) measurment.");
+        }
+        print_newline();
+        free_tree(&tree);
+    }
     print_newline();
-    print_string("Min Depth: ");
-    print_int(min_depth(tree));
+    print_string("Testing balanced trees of values ");
+    for(int i = 0; i < TEST_SIZE; i++) {
+        print_int(tree_size[i]);
+        if(i != TEST_SIZE - 1)
+            print_string(", ");
+        else
+            print_string(".");
+    }
     print_newline();
-    balance_tree(&tree);
-    print_string("Preorder: ");
-    print_preorder(tree);
-    print_newline();
-    print_string("Depth: ");
-    print_int(depth(tree));
-    print_newline();
-    print_string("Min Depth: ");
-    print_int(min_depth(tree));
+    first = 0;
+    for(int i = 0; i < TEST_SIZE; i++) {
+        print_string("Creating tree for tree size: ");
+        print_int(tree_size[i]);
+        print_newline();
+        for(int j = 1; j <= tree_size[i]; j++) {
+            insert_sorted(&tree, j);
+        }
+        print_string("Balancing tree...");
+        print_newline();
+        balance_tree(&tree);
+        print_string("Starting search for ");
+        print_int(tree_size[i]);
+        print_string(" in tree...");
+        print_newline();
+        nrf_systick_val_clear();
+        unsigned int pretime = nrf_systick_val_get();
+        find(tree, tree_size[i]);
+        unsigned int posttime = nrf_systick_val_get();
+        print_string("Value found after ");
+        print_int((pretime - posttime));
+        print_string(" ticks!");
+        if(first == 0)
+        {
+            first = (pretime - posttime);
+            print_string(" ... which is around ");
+            print_int((unbalanced_speed[i])/(int)(pretime - posttime));
+            print_string("x faster than the unbalanced counterpart of the same tree.");
+        }
+        else
+        {
+            print_string(" ... which is around ");
+            print_int(ceil((pretime - posttime)/(double)first));
+            print_string("x slower compared to the first (balanced) measurment and ");
+            print_int((unbalanced_speed[i])/(int)(pretime - posttime));
+            print_string("x faster than the unbalanced counterpart of the same tree.");
+        }
+        print_newline();
+        free_tree(&tree);
+    }
     print_newline();
 }
