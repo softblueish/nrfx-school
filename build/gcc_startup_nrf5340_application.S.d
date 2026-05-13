2964ec53a691c7c1832397fcce4a0f4a01a83a77
softblueish <stoplooking@atmy.email>
2026-05-13 09:48:46 +0200

    Finally done

---
+++ build/gcc_startup_nrf5340_application.S.d
@@ -0,0 +1,2 @@
+H:/Code/nrfx/src/sample/../../build/gcc_startup_nrf5340_application.S.o: \
+ H:/Code/nrfx/mdk/gcc_startup_nrf5340_application.S
--- src/sample/main.c
+++ src/sample/main.c
@@ -35,20 +35,33 @@ LED 4       P0.31
 #define PIN_RXD 22
 #define CLEAR_SCREEN "\033c"
 
+#define LED1 (28)
+#define LED2 (29)
+#define LED3 (30)
+#define LED4 (31)
 #define BUTTON1 (23)
 #define BUTTON2 (24)
 #define BUTTON3 (8)
 #define BUTTON4 (9)
+#define LED_ON 0
+#define LED_OFF 1
 
 const nrfx_rtc_t rtc_instance = NRFX_RTC_INSTANCE(0);
 
+const int LED[] = {LED1, LED2, LED3, LED4};
+
 int main(void){
     int srandUsed = 0;
+    init_uarte();
     nrfx_rtc_config_t rtc_config = NRFX_RTC_DEFAULT_CONFIG;
     nrfx_rtc_init(&rtc_instance, &rtc_config, NULL);
     nrfx_rtc_enable(&rtc_instance);
     nrfx_systick_init();
 
+    nrf_gpio_cfg_output(LED1);
+    nrf_gpio_cfg_output(LED2);
+    nrf_gpio_cfg_output(LED3);
+    nrf_gpio_cfg_output(LED4);
     nrf_gpio_cfg_input(BUTTON1, NRF_GPIO_PIN_PULLUP);
     nrf_gpio_cfg_input(BUTTON2, NRF_GPIO_PIN_PULLUP);
     nrf_gpio_cfg_input(BUTTON3, NRF_GPIO_PIN_PULLUP);
@@ -57,13 +70,35 @@ int main(void){
     clear_screen();
     print_string("Program started, please press any button to initialize the program.\r\n");
     
+    for(int i = 0 ; i < 4; i++)
+        nrf_gpio_pin_write(LED[i], LED_OFF);
     while(1){
         if(!nrf_gpio_pin_read(BUTTON1) || !nrf_gpio_pin_read(BUTTON2) || !nrf_gpio_pin_read(BUTTON3) || !nrf_gpio_pin_read(BUTTON4)){
             if(!srandUsed){
                 srand(nrfx_rtc_counter_get(&rtc_instance));
                 srandUsed = 1;
-                print_string("Program initialized with a seed value.\r\n");
+                break;
             }
+        }
+    }
+    print_string("Program initialized with a seed value.\r\n");
+    while(1){
+        int amount = 0;
+        int delay = 0;
+        print_string("Enter an amount: ");
+        read_int(&amount);
+        print_newline();
+        print_string("Enter a delay: ");
+        read_int(&delay);
+        print_newline();
+        print_string("Executing program, it'll take ~");
+        print_int(amount * delay);
+        print_string("ms\r\n");
+        for(int i = 0; i < amount; i++){
+            int selected_led = rand() % 4;
+            nrf_gpio_pin_write(LED[selected_led], LED_ON);
+            nrfx_systick_delay_ms(delay);
+            nrf_gpio_pin_write(LED[selected_led], LED_OFF);
         }
     }
 }
